45615f2afaf85fe535f390fd54cd44ca66b85c68
sujaykumar51 <sujaykumar51@gmail.com>
2021-08-04 22:41:52 +0530

    employee adding script

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -27,115 +27,304 @@ class Home extends CI_Controller {
 
 	public function index()
 	{
-		$designation = []; //hs_hr_job_title - 3
-		$job_titles = []; //4 hs_hr_compstructtree
+		print_r("Uploading start ");
 
-
-		$work_station_id = 0;
-		$designation_id = 0;
-
-		$work_station = $this->excel_import_model->getWorkStation();
-		foreach($work_station as $row_wk){
-			if(!empty($row_wk->jobtit_name_si)){
-				$job_titles[$row_wk->jobtit_name_si] = $row_wk->jobtit_code;
-				$work_station_id = $work_station_id < intval(str_replace("JOB","",$row_wk->jobtit_code)) ? intval(str_replace("JOB","",$row_wk->jobtit_code)) : $work_station_id;
-			}			
-		}
-
-		$designation_table = $this->excel_import_model->getDesignation();
-		foreach($designation_table as $row_wk){
-			if(!empty($row_wk->title_si)){
-				$designation[$row_wk->title_si] = $row_wk->id;
-				$designation_id = $designation_id < $row_wk->id ? $row_wk->id : $designation_id;
-			}			
-		}
-		
-
-		
-
-		
-
-
-		$path = FCPATH."upload/data.xlsx";
-		$object = PHPExcel_IOFactory::load($path);
-		foreach($object->getWorksheetIterator() as $worksheet)
-		{
-			
-			$highestRow = $worksheet->getHighestRow();
-			$highestColumn = $worksheet->getHighestColumn();
-			for($row=2; $row<=$highestRow; $row++)
+		for($x = 1; $x <=11; $x++){
+			$path = FCPATH."upload/".$x.".xlsx";
+			$object = PHPExcel_IOFactory::load($path);
+			foreach($object->getWorksheetIterator() as $key => $worksheet)
 			{
-
-				$data['emp_number'] = $worksheet->getCellByColumnAndRow(0, $row)->getValue();
-				$data['emp_salary_no'] = $worksheet->getCellByColumnAndRow(1, $row)->getValue();
-				$data['emp_lastname_si'] = $worksheet->getCellByColumnAndRow(2, $row)->getValue();
-				//$job_title_code = $worksheet->getCellByColumnAndRow(3, $row)->getValue(); //code to convert
-				//$hs_hr_compstructtree = $worksheet->getCellByColumnAndRow(4, $row)->getValue();  //code to convert
-				$data['emp_app_date'] = $worksheet->getCellByColumnAndRow(7, $row)->getValue();
-				$data['emp_nic_no'] = strtoupper($worksheet->getCellByColumnAndRow(11, $row)->getValue());
-				$data['employee_id'] = strtoupper($worksheet->getCellByColumnAndRow(11, $row)->getValue());
-
-				$data['employee_id'] = !empty($data['employee_id']) ? $data['employee_id'] : null;
-
-				$service_temp = trim($worksheet->getCellByColumnAndRow(3, $row)->getValue());
-				$service_temp = str_replace("(","-",$service_temp);
-				$service_temp = str_replace(")","",$service_temp);
-
-				$service_pos_temp = $worksheet->getCellByColumnAndRow(6, $row)->getValue();
-
-				
-				$data['job_title_code'] = NULL;
-				$data['work_station'] = NULL;
-				if(!empty($service_temp)){
-					if(!empty($job_titles[$service_temp])){
-						$data['job_title_code'] = $job_titles[$service_temp];
-					}else{
-						$work_station_id = ++$work_station_id;
-						$this->excel_import_model->insertJobTitle(['jobtit_code'=>"JOB".$work_station_id, 'jobtit_name'=>$service_temp,'jobtit_name_si'=>$service_temp, 'jobtit_name_ta'=>$service_temp]);
-						
-						$data['job_title_code'] = "JOB".$work_station_id;
-						$job_titles[$service_temp] = "JOB".$work_station_id;
-					}
+				echo "<pre>";
+					print_r("excel ".$x." sheet ".$key);
+				echo "</pre>";
+				if($key==0){
+					$this->add_sheet_1($worksheet);
+				}else if($key==1){
+					$this->add_address_sheet($worksheet);
+				}else if($key==2){
+					$this->add_dependent_sheet($worksheet);
+				}else if($key==3){
+					$this->add_emergency_sheet($worksheet);
+				}else if($key==4){
+					$this->add_job_sheet($worksheet);
+				}else if($key==5){
+					//$this->add_service_sheet($worksheet);
+				}else if($key==6){
+					//$this->add_education_sheet($worksheet);
+				}else if(false && $key==7){
+					//$this->add_job_sheet($worksheet);
+				}else if(false && $key==8){
+					//$this->add_job_sheet($worksheet);
+				}else if(false && $key==9){
+					//$this->add_job_sheet($worksheet);
+				}else if(false && $key==10){
+					//$this->add_job_sheet($worksheet);
+				}else if(false && $key==11){
+					//$this->add_job_sheet($worksheet);
+				}else if(false && $key==12){
+					//$this->add_job_sheet($worksheet);
 				}
-
-				$service_pos_temp = str_replace("(","-",$service_pos_temp);
-				$service_pos_temp = str_replace(")","",$service_pos_temp);
-				if(!empty($service_pos_temp)){
-					if(!empty($designation[$service_pos_temp])){
-						$data['work_station'] = $designation[$service_pos_temp];
-					}else{
-						++$designation_id;
-						$this->excel_import_model->insertWorkStation(['id'=>$designation_id,'comp_code'=>$designation_id,'title_si'=>$service_pos_temp, 'title'=>$service_pos_temp]);
-						
-						$data['work_station'] = $designation_id;
-						$designation[$service_pos_temp] = $designation_id;
-					}
-				}
-
-				$data['emp_display_name'] = $worksheet->getCellByColumnAndRow(2, $row)->getValue();
-				
-				if($data['employee_id']=="ගුවන් හමුදාව"){
-					$data['employee_id'] = NULL;
-					$data['emp_nic_no'] = NULL;
-				}
-				if($data['employee_id']=="යුධ හමුදා"){
-					$data['employee_id'] = NULL;
-					$data['emp_nic_no'] = NULL;
-				}
-				if($data['employee_id']=="නාවික හමුදාව"){
-					$data['employee_id'] = NULL;
-					$data['emp_nic_no'] = NULL;
-				}
-				
-				if(!empty($data['emp_number'])){
-					echo "<pre>";
-					print_r($service_pos_temp);
-					print_r($data);
-					$this->excel_import_model->insertData($data);
-				}
-				
 			}
 		}
+		
+
+
+		
 		echo 'Data Imported successfully';
 	}
+
+	public function add_sheet_1($worksheet){
+		$highestRow = $worksheet->getHighestRow();
+		$highestColumn = $worksheet->getHighestColumn();
+		for($row=4; $row<=$highestRow; $row++)
+		{
+
+			$data['emp_num_pso'] = $worksheet->getCellByColumnAndRow(0, $row)->getValue();
+			//$data['emp_number'] = $this->empid;//$worksheet->getCellByColumnAndRow(1, $row)->getValue();
+			$data['employee_id'] = $worksheet->getCellByColumnAndRow(1, $row)->getValue();
+			$data['emp_firstname'] = strtoupper($worksheet->getCellByColumnAndRow(8, $row)->getValue());
+			$data['emp_lastname'] = strtoupper($worksheet->getCellByColumnAndRow(9, $row)->getValue());
+			$data['emp_birthday'] = strtoupper($worksheet->getCellByColumnAndRow(10, $row)->getValue());
+			$data['gender_code'] = strtoupper($worksheet->getCellByColumnAndRow(5, $row)->getValue());
+			if(empty($data['employee_id'])){
+				break;
+			}
+			//$data['employee_id'] = strtoupper($worksheet->getCellByColumnAndRow(11, $row)->getValue());
+
+			//$data['employee_id'] = !empty($data['employee_id']) ? $data['employee_id'] : null;
+
+			//$service_temp = trim($worksheet->getCellByColumnAndRow(3, $row)->getValue());
+			//$service_temp = str_replace("(","-",$service_temp);
+			//$service_temp = str_replace(")","",$service_temp);
+
+			//$service_pos_temp = $worksheet->getCellByColumnAndRow(6, $row)->getValue();
+
+			
+			//print_r($data);
+			//die();
+
+			
+			
+			if(!empty($data['employee_id'])){
+				echo "<pre>";
+				print_r($row." ".$data['employee_id']. " creating");
+				print_r($data);
+				echo "</pre>";
+				//print_r($data);
+				$this->excel_import_model->insertData($data);
+			}
+			
+		}
+	}
+
+	
+	public function add_address_sheet($worksheet){
+		$highestRow = $worksheet->getHighestRow();
+		$highestColumn = $worksheet->getHighestColumn();
+		for($row=4; $row<=$highestRow; $row++)
+		{
+			$employee_id = $worksheet->getCellByColumnAndRow(0, $row)->getValue();
+			$data['emp_number'] = $this->excel_import_model->getemp_number($employee_id);
+			$data['con_per_addLine1'] = $worksheet->getCellByColumnAndRow(1, $row)->getValue();
+			$data['con_res_addLine1'] = $worksheet->getCellByColumnAndRow(2, $row)->getValue();
+			$data['con_per_del_postoffice'] = $worksheet->getCellByColumnAndRow(3, $row)->getValue();
+			$data['con_per_postal_code'] = $worksheet->getCellByColumnAndRow(4, $row)->getValue();
+			$data['con_per_district'] = $worksheet->getCellByColumnAndRow(5, $row)->getValue();
+			$data['con_per_div_sectretariat'] = $worksheet->getCellByColumnAndRow(6, $row)->getValue();
+			$data['con_per_policesation'] = $worksheet->getCellByColumnAndRow(7, $row)->getValue();
+			$data['con_per_phone'] = $worksheet->getCellByColumnAndRow(8, $row)->getValue();
+			$data['con_per_mobile'] = $worksheet->getCellByColumnAndRow(9, $row)->getValue();
+			$data['con_per_fax'] = $worksheet->getCellByColumnAndRow(11, $row)->getValue();
+			$data['con_per_email'] = $worksheet->getCellByColumnAndRow(14, $row)->getValue();
+
+
+			if(empty($data['emp_number'])){
+				break;
+			}
+
+			
+			
+			if(!empty($data['emp_number'])){
+				echo "<pre>";
+				print_r($row." ".$data['emp_number']. " address creating");
+				print_r($data);
+				echo "</pre>";
+				$this->excel_import_model->insertAddressData($data);
+			}
+			
+		}
+	}
+
+	public function add_dependent_sheet($worksheet){
+		$highestRow = $worksheet->getHighestRow();
+		$highestColumn = $worksheet->getHighestColumn();
+		for($row=5; $row<=$highestRow; $row++)
+		{
+			$employee_id = $worksheet->getCellByColumnAndRow(0, $row)->getValue();
+			$data['emp_number'] = $this->excel_import_model->getemp_number($employee_id);
+			$data['ed_name'] = $worksheet->getCellByColumnAndRow(2, $row)->getValue();
+			$data['ed_nic'] = $worksheet->getCellByColumnAndRow(5, $row)->getValue();
+			$data['rel_code'] = $worksheet->getCellByColumnAndRow(11, $row)->getValue();
+			$data['ed_birthday'] = $worksheet->getCellByColumnAndRow(9, $row)->getValue();
+			$data['ed_workplace'] = $worksheet->getCellByColumnAndRow(7, $row)->getValue();
+			$data['ed_address'] = $worksheet->getCellByColumnAndRow(4, $row)->getValue();
+			//$data['ed_comments'] = $worksheet->getCellByColumnAndRow(7, $row)->getValue();
+
+			if(empty($data['ed_nic'])){
+				continue;
+			}
+
+			if(empty($data['emp_number'])){
+				break;
+			}
+
+			
+			
+			if(!empty($data['emp_number'])){
+				echo "<pre>";
+				print_r($row." ".$data['emp_number']. " address creating");
+				print_r($data);
+				echo "</pre>";
+				$this->excel_import_model->insertDependentData($data);
+			}
+			
+		}
+	}
+
+	public function add_emergency_sheet($worksheet){
+		$highestRow = $worksheet->getHighestRow();
+		$highestColumn = $worksheet->getHighestColumn();
+		for($row=5; $row<=$highestRow; $row++)
+		{
+			$employee_id = $worksheet->getCellByColumnAndRow(0, $row)->getValue();
+			$data['emp_number'] = $this->excel_import_model->getemp_number($employee_id);
+			//$data['eec_seqno'] = $worksheet->getCellByColumnAndRow(2, $row)->getValue();
+			$data['eec_name'] = $worksheet->getCellByColumnAndRow(1, $row)->getValue();
+			$data['eec_relationship'] = $worksheet->getCellByColumnAndRow(2, $row)->getValue();
+			$data['eec_address'] = $worksheet->getCellByColumnAndRow(3, $row)->getValue();
+			$data['eec_home_no'] = $worksheet->getCellByColumnAndRow(4, $row)->getValue();
+			$data['eec_office_no'] = $worksheet->getCellByColumnAndRow(5, $row)->getValue();
+			$data['eec_mobile_no'] = $worksheet->getCellByColumnAndRow(6, $row)->getValue();
+
+			if(empty($employee_id)){
+				break;
+			}
+			if(empty($data['emp_number'])){
+				continue;
+			}
+
+			
+			
+			if(!empty($data['emp_number'])){
+				echo "<pre>";
+				print_r($row." ".$data['emp_number']. " address creating ".$employee_id);
+				print_r($data);
+				echo "</pre>";
+				$this->excel_import_model->insertEmergencyData($data);
+			}
+			
+		}
+	}
+
+	public function add_job_sheet($worksheet){
+		$highestRow = $worksheet->getHighestRow();
+		$highestColumn = $worksheet->getHighestColumn();
+
+		$job_title = $this->excel_import_model->getjob_title();
+		$job_title_id = 0;
+		foreach($job_title as $row_wk){
+			if(!empty($row_wk->jobtit_name)){
+				$job_titles[$row_wk->jobtit_name] = $row_wk->jobtit_code;
+				$job_title_id = $job_title_id < intval(str_replace("JOB","",$row_wk->jobtit_code)) ? intval(str_replace("JOB","",$row_wk->jobtit_code)) : $job_title_id;
+			}			
+		}
+
+
+		for($row=5; $row<=$highestRow; $row++)
+		{
+			$employee_id = $worksheet->getCellByColumnAndRow(0, $row)->getValue();
+			$data['emp_number'] = $this->excel_import_model->getemp_number($employee_id);
+			$job = $worksheet->getCellByColumnAndRow(2, $row)->getValue();
+
+			if(!empty($job) && $job != NULL){					
+				if(!empty($job_titles[$job])){
+					$data['job_title_code'] = $job_titles[$job];
+				}else{
+					$job_title_id = ++$job_title_id;
+					$this->excel_import_model->insertJobTitle(['jobtit_code'=>"JOB".$job_title_id, 'jobtit_name'=>$job,'jobtit_name_si'=>$job, 'jobtit_name_ta'=>$job]);
+					
+					$data['job_title_code'] = "JOB".$job_title_id;
+					$job_titles[$job] = "JOB".$job_title_id;
+				}
+			}else{
+				$data['job_title_code'] = "";
+			}
+			
+			$data['emp_app_date'] = $worksheet->getCellByColumnAndRow(6, $row)->getValue();
+			$data['emp_com_date'] = $worksheet->getCellByColumnAndRow(7, $row)->getValue();
+			$data['emp_basic_salary'] = $worksheet->getCellByColumnAndRow(18, $row)->getValue();
+			$data['emp_salary_inc_date'] = $worksheet->getCellByColumnAndRow(19, $row)->getValue();
+
+			if(empty($employee_id)){
+				break;
+			}
+			if(empty($data['emp_number'])){
+				continue;
+			}
+
+			
+			
+			if(!empty($data['emp_number'])){
+				echo "<pre>";
+				print_r($row." ".$data['emp_number']. " address creating ".$employee_id);
+				print_r($data);
+				echo "</pre>";
+				$this->excel_import_model->insertData($data);
+			}
+			
+		}
+	}
+
+	public function add_education_sheet($worksheet){
+		$highestRow = $worksheet->getHighestRow();
+		$highestColumn = $worksheet->getHighestColumn();
+
+		$job_title = $this->excel_import_model->getjob_title();
+		$job_title_id = 0;
+		foreach($job_title as $row_wk){
+			if(!empty($row_wk->jobtit_name)){
+				$job_titles[$row_wk->jobtit_name] = $row_wk->jobtit_code;
+				$job_title_id = $job_title_id < intval(str_replace("JOB","",$row_wk->jobtit_code)) ? intval(str_replace("JOB","",$row_wk->jobtit_code)) : $job_title_id;
+			}			
+		}
+
+
+		for($row=5; $row<=$highestRow; $row++)
+		{
+			$employee_id = $worksheet->getCellByColumnAndRow(0, $row)->getValue();
+			$data['emp_number'] = $this->excel_import_model->getemp_number($employee_id);			
+			$data['edu_type_id'] = $worksheet->getCellByColumnAndRow(1, $row)->getValue();
+			$data['grd_year'] = $worksheet->getCellByColumnAndRow(4, $row)->getValue();
+
+			if(empty($employee_id)){
+				break;
+			}
+			if(empty($data['emp_number'])){
+				continue;
+			}
+
+			
+			
+			if(!empty($data['emp_number'])){
+				echo "<pre>";
+				print_r($row." ".$data['emp_number']. " address creating ".$employee_id);
+				print_r($data);
+				echo "</pre>";
+				$this->excel_import_model->insertEducationData($data);
+			}
+			
+		}
+	}
+
+	
 }
--- application/models/Excel_import_model.php
+++ application/models/Excel_import_model.php
@@ -1,7 +1,7 @@
 <?php
 class Excel_import_model extends CI_Model {
     
-    public function getWorkStation($data = [])
+    public function getjob_title($data = [])
     {
         $query = $this->db->select('*')
             ->where($data)
@@ -32,6 +32,14 @@ class Excel_import_model extends CI_Model {
         $insert_id = $this->db->insert_id();
     }
 
+    public function getemp_number($employee_id)
+    {
+        $query = $this->db->select('emp_number')
+                        ->where('employee_id', $employee_id)
+                        ->from('hs_hr_employee')
+                        ->get()->result_array();
+        return !empty($query[0]['emp_number']) ? $query[0]['emp_number'] : 0;
+    }
     
 
     
@@ -40,55 +48,177 @@ class Excel_import_model extends CI_Model {
     {
         $num_row = 0;
         $is_added = false;
-        if (!empty($data['emp_number'])){
-            $query = $this->db->select('employee_id')
-                        ->where('emp_number', $data['emp_number'])
-                        ->from('hs_hr_employee')
-                        ->get();
-            $num_row = $query->num_rows();
-            if($num_row > 0){
-                $this->db->set($data);
-                $this->db->where('emp_number', $data['emp_number']);
-                $restule = $this->db->update('hs_hr_employee');
-                $is_added = true;
-            }
-
-        } else if(!empty($data['employee_id'])){
+        if (!empty($data['employee_id'])){
             $query = $this->db->select('employee_id')
                         ->where('employee_id', $data['employee_id'])
                         ->from('hs_hr_employee')
                         ->get();
             $num_row = $query->num_rows();
-            
             if($num_row > 0){
                 $this->db->set($data);
                 $this->db->where('employee_id', $data['employee_id']);
                 $restule = $this->db->update('hs_hr_employee');
                 $is_added = true;
             }
-        }else if (!empty($data['emp_salary_no'])){
-            $query = $this->db->select('employee_id')
-                        ->where('emp_salary_no', $data['emp_salary_no'])
-                        ->from('hs_hr_employee')
-                        ->get();
-            $num_row = $query->num_rows();
-            
-            if($num_row > 0){
-                $this->db->set($data);
-                $this->db->where('emp_salary_no', $data['emp_salary_no']);
-                $restule = $this->db->update('hs_hr_employee');
-                $is_added = true;
+
+        }
+        if(!$is_added){ 
+            if (!empty($data['emp_number'])){
+                $query = $this->db->select('emp_number')
+                            ->where('emp_number', $data['emp_number'])
+                            ->from('hs_hr_employee')
+                            ->get();
+                $num_row = $query->num_rows();
+                if($num_row > 0){
+                    $this->db->set($data);
+                    $this->db->where('emp_number', $data['emp_number']);
+                    $restule = $this->db->update('hs_hr_employee');
+                    $is_added = true;
+                }
+
             }
-        }  
+        }
 
         
                   
-        if(!$is_added){            
+        if(!$is_added){          
             $this->db->insert('hs_hr_employee', $data);
+
+              
+            $insert_id = $this->db->insert_id();
+            $data1['emp_number'] = $insert_id;
+            $this->db->set($data1);
+            $this->db->where('employee_id', $data['employee_id']);
+            $restule = $this->db->update('hs_hr_employee');
+            $is_added = true;
         }
 
         echo "find rown".$num_row."\n";
         echo "-------------------------------\n\n\n";
     }
 
+
+    
+
+    public function insertAddressData($data)
+    {
+        $num_row = 0;
+        $is_added = false;
+        if (!empty($data['emp_number'])){
+            $query = $this->db->select('emp_number')
+                        ->where('emp_number', $data['emp_number'])
+                        ->from('hs_hr_emp_contact')
+                        ->get();
+            $num_row = $query->num_rows();
+            if($num_row > 0){
+                $this->db->set($data);
+                $this->db->where('emp_number', $data['emp_number']);
+                $restule = $this->db->update('hs_hr_emp_contact');
+                $is_added = true;
+            }
+
+        }
+
+        
+                  
+        if(!$is_added){          
+            $this->db->insert('hs_hr_emp_contact', $data);
+            $is_added = true;
+        }
+
+        echo "find rown".$num_row."\n";
+        echo "-------------------------------\n\n\n";
+    }
+    public function insertDependentData($data)
+    {
+        $num_row = 0;
+        $is_added = false;
+        if (!empty($data['emp_number'])){
+            $query = $this->db->select('emp_number')
+                        ->where('emp_number', $data['emp_number'])
+                        ->from('hs_hr_emp_dependents')
+                        ->get();
+            $num_row = $query->num_rows();
+            if($num_row > 0){
+                $this->db->set($data);
+                $this->db->where('emp_number', $data['emp_number']);
+                $restule = $this->db->update('hs_hr_emp_dependents');
+                $is_added = true;
+            }
+
+        }
+
+        
+                  
+        if(!$is_added){          
+            $this->db->insert('hs_hr_emp_dependents', $data);
+            $is_added = true;
+        }
+
+        echo "find rown".$num_row."\n";
+        echo "-------------------------------\n\n\n";
+    }
+    
+    public function insertEmergencyData($data)
+    {
+        $num_row = 0;
+        $is_added = false;
+        if (!empty($data['emp_number'])){
+            $query = $this->db->select('emp_number')
+                        ->where('emp_number', $data['emp_number'])
+                        ->from('hs_hr_emp_emergency_contacts')
+                        ->get();
+            $num_row = $query->num_rows();
+            if($num_row > 0){
+                $this->db->set($data);
+                $this->db->where('emp_number', $data['emp_number']);
+                $restule = $this->db->update('hs_hr_emp_emergency_contacts');
+                $is_added = true;
+            }
+
+        }
+
+        
+                  
+        if(!$is_added){          
+            $this->db->insert('hs_hr_emp_emergency_contacts', $data);
+            $is_added = true;
+        }
+
+        echo "find rown".$num_row."\n";
+        echo "-------------------------------\n\n\n";
+    }
+
+
+    public function insertEducationData($data)
+    {
+        $num_row = 0;
+        $is_added = false;
+        if (!empty($data['emp_number'])){
+            $query = $this->db->select('emp_number')
+                        ->where('emp_number', $data['emp_number'])
+                        ->from('hs_hr_edu_emp_head')
+                        ->get();
+            $num_row = $query->num_rows();
+            if($num_row > 0){
+                $this->db->set($data);
+                $this->db->where('emp_number', $data['emp_number']);
+                $restule = $this->db->update('hs_hr_edu_emp_head');
+                $is_added = true;
+            }
+
+        }
+
+        
+                  
+        if(!$is_added){          
+            $this->db->insert('hs_hr_edu_emp_head', $data);
+            $is_added = true;
+        }
+
+        echo "find rown".$num_row."\n";
+        echo "-------------------------------\n\n\n";
+    }
+
+    
 }
